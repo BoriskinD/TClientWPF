9581e647ca3773a7dbfda1e2beae04ce7c3303a0
BoriskinD <shark9797@mail.ru>
2023-10-26 14:05:59 +0300

    Изменения

--- TClientWPF/Services/Logger.cs
+++ TClientWPF/Services/Logger.cs
@@ -5,14 +5,22 @@ using System.Text;
 
 namespace TClientWPF.Services
 {
+    //Реализует Одиночку
     public class Logger : INotifyPropertyChanged
     {
         private StringBuilder log;
+        private static Logger instance;
         public event PropertyChangedEventHandler PropertyChanged;
 
         public string Log => log.ToString();
 
-        public Logger() => log = new StringBuilder();
+        private Logger() => log = new StringBuilder();
+
+        public static Logger GetInstance()
+        {
+            if (instance == null) instance = new Logger();
+            return instance;
+        }
 
         public void AddText(string text)
         {
